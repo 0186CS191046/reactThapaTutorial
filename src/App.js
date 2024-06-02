b484d7832e0c914aa2bd966674e7fd5ec0c6c79f
0186CS191046 <kumarikajal89681@gmail.com>
2024-06-02 11:44:22 +0530

    greeting mini-project convert to function component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
-import Heading from "./Heading";
-import Para from "./Para";
-import List from "./List";
+import { createRoot } from "react-dom/client";
+import './index.css'
 
-function App(){
-    return (
+function App() {
+    let greeting = ''
+    let cssStyle = {}
+    let currDate = new Date(2024, 6, 2, 12)
+    let currGreeting = currDate.getHours()
+    if (currGreeting >= 1 && currGreeting < 12) {
+        greeting = "Good Morning"
+        cssStyle.color = 'red'
+    }
+    else if (currGreeting <= 12 && currGreeting < 19) {
+        greeting = 'Good Afternoon'
+        cssStyle.color = 'orange'
+    } else {
+        greeting = 'Good Night'
+        cssStyle.color = 'black'
+    }
+
+    createRoot(document.getElementById('root')).render(
         <>
-        <Heading/>
-        <Para/>
-        <List/>
+            <div>
+                <h1>Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
+            </div>
         </>
     )
 }
