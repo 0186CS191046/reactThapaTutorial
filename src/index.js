996f6b1254c5c8f19122ab86e8bde73e96132c61
0186CS191046 <kumarikajal89681@gmail.com>
2024-06-02 12:28:53 +0530

    Adding Calculator challenge

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import youTuber ,{Myname,course,myTitle }from './App'
+// import youTuber ,{Myname,course,myTitle }from './App'
+import add ,{sub,mul,div }from './Calc'
 
 createRoot(document.getElementById('root')).render(
-    <ol>
-        <li>Kajal</li>
-        <li>{youTuber}</li>
-        <li>{course}</li>
-        <li>{Myname()}</li>
-        <li>{myTitle()}</li>
-    </ol>
+    <>
+   <ul>
+    <li>Sum of two no is {add(30,3)}</li>
+    <li>Sub of two no is {sub(30,3)}</li>
+    <li>Mul of two no is {mul(30,3)}</li>
+    <li>Div of two no is {div(30,3)}</li>
+   </ul>
+   </>
 )
