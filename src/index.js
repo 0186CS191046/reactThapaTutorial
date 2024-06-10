17e097aee3744090f13ed9e6921f18c7b634efea
0186CS191046 <kumarikajal89681@gmail.com>
2024-06-10 22:50:54 +0530

    adding netflix webseries css

--- src/index.js
+++ src/index.js
@@ -4,33 +4,20 @@ import  './index.css'
 import Cards from "./Cards";
 import data from "./netflixdata.jsx";
 console.log(data)
+
+function nCard(val){
+    console.log(val);
+    return(
+<Cards  imgsrc= {val.imgsrc}
+     title={val.title}
+     sname={val.sname}
+     links={val.links}
+     />
+    )
+}
 createRoot(document.getElementById('root')).render(
     <>
-    <div id="main">
-    <Cards  imgsrc= {data[0].imgsrc}
-     title={data[0].title}
-     sname={data[0].sname}
-     links={data[0].links}
-     />
-    <Cards  sname=  {data[1].sname}
-        imgsrc= {data[1].imgsrc}
-        title={data[1].title}
-        links={data[1].links}
-        />
-    <Cards sname = {data[2].sname}
-        imgsrc = {data[2].imgsrc}
-        title={data[2].title}
-        links={data[2].links}/>
-
-<Cards sname = {data[3].sname}
-        imgsrc = {data[3].imgsrc}
-        title={data[3].title}
-        links={data[3].links}/>
-
-<Cards sname = {data[4].sname}
-        imgsrc = {data[4].imgsrc}
-        title={data[4].title}
-        links={data[4].links}/>
-        </div>
+    <h1 className="heading_style">List of top 5 Netflix webseries</h1>
+     {data.map(nCard)}
     </>  
 )
