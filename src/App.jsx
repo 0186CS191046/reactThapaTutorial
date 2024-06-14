ebe5a0a74c3b090171d51acfc17dc21251c97580
0186CS191046 <kumarikajal89681@gmail.com>
2024-06-14 10:23:03 +0530

    netflix data with ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,14 @@ import React from "react";
 import Netflix from "./Netflix";
 import Amazon from "./Amazon";
 
-const favSeries = 'amazon'
-
-const FavS = ()=>{
-if (favSeries === 'netflix') {
-    return <Netflix/>
-}
-else{
-    return <Amazon/>
-}}
+const favSeries = 'netflix'
 
 const App = () => {
     return (
         <>
             <h1 className="heading_style">List of top 5 Netflix webseries</h1>
             <div id="main">
-                <FavS/>
+               {(favSeries == 'netflix')?<Netflix/>:<Amazon/>}
             </div>
         </>
     )
