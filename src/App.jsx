e5576bef0a38a642b6a3d9b225beef6ff995711c
0186CS191046 <kumarikajal89681@gmail.com>
2024-06-24 22:47:52 +0530

    form with name and password

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
 
-const App = ()=> {
+const App = () => {
     const [name, setname] = useState()
     const [fullname, setFullname] = useState()
-    const textChange = (event)=> {
+    const [password, setpassword] = useState()
+    const [newpassword, setnewpassword] = useState()
+
+    const textChange = (event) => {
         setname(event.target.value)
     }
-    const submitText = ()=> {
+    const passChange = (event) => {
+        setpassword(event.target.value)
+    }
+    const submitText = (event) => {
+        event.preventDefault();
         setFullname(name)
+        setnewpassword(password)
 
     }
-    return(
+    return (
         <>
-        <div className="container">
-            <h1>Hello {fullname}</h1>
-            <input type="text" placeholder="Enter Your Name" defaultValue ="" value={name} onChange={textChange}/>
-            <button onClick={submitText}>Submit 👍</button>
-        </div>
+            <div className="container">
+                <form onSubmit={submitText}>
+                <div class="main_div">
+                <h1>Hello {fullname} {newpassword}</h1>
+                
+                    <input type="text" placeholder="Enter Your Name" defaultValue="" value={name} onChange={textChange} />
+                    <input type="text" placeholder="Enter Your Password" defaultValue="" value={password} onChange={passChange} />
+                    <button >Submit 👍</button>
+                    </div>
+                    </form>
+            </div>
+
         </>
     )
 }
