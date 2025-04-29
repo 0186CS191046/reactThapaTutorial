a422617a6b2eaea1a7518f7bcb1d6fbda72dab22
0186CS191046 <kajal.kumari@ens.enterprises>
2025-04-29 22:15:21 +0530

    accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,20 @@
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import AddIcon from '@mui/icons-material/Add';
+import { questions } from "./questions";
 import "./index.css"
-import Item from "./Item";
+import Accordion from "./Accordion";
 
 const App = () => {
-  const [item, setItem] = useState('')
-  const [arr, setArr] = useState([])
-
-  const cutIt = (index) => {
-    setArr((preValue) => {
-      return preValue.map((value, id) => {
-        return id === index ? { ...value, isCut: true } : value;
-      });
-    });
-  };
-
-  const setField = (e) => {
-    setItem(e.target.value)
-  }
-
-  const addData = (e) => {
-    setArr((preValue) => {
-      return [...preValue, { text: item, isCut: false }]
-    })
-    setItem("")
-  }
+  console.log("--", questions);
 
   return (
     <>
-      <div className="main_div">
-        <div className="center_div">
-          <br />
-          <h1>ToDo List</h1>
-          <input type="text" placeholder="Add an Item" onChange={setField} value={item} />
-          <Button className="btn" > <AddIcon onClick={addData} /> </Button>
-
-          <ul>
-            {arr.map((ar, index) => {
-              {/* return <li>{ar}</li> */ }
-              return <Item key={index} item={ar.text} cutIt={()=>cutIt(index)} line={ar.isCut} />
-            })}
-          </ul>
-        </div>
-      </div>
+      <section className="main_div">
+        <h1>React interview questions</h1>
+{questions.map((element)=>{
+  {/* console.log("element",element) */}
+  return <Accordion question = {element.question} answer={element.answers}/>
+})}
+      </section>
     </>
   )
 }
---
+++ src/questions.js
@@ -0,0 +1,27 @@
+export const questions = [
+    {
+        "id":1,
+        "question":"What is React?",
+        "answers":"React is a front-end and open-source JavaScript library which is useful in developing user interfaces specifically for applications with a single page. It is helpful in building complex and reusable user interface(UI) components of mobile and web applications as it follows the component-based approach."
+    },
+    {
+        "id":2,
+        "question":"What is useState() in React?",
+        "answers":"The useState() is a built-in React Hook that allows you for having state variables in functional components. It should be used when the DOM has something that is dynamically manipulating/controlling."
+    },
+    {
+        "id":3,
+        "question":"What is JSX?",
+        "answers":"JSX stands for JavaScript XML. It allows us to write HTML inside JavaScript and place them in the DOM without using functions like appendChild( ) or createElement( )."
+    },
+    {
+        "id":4,
+        "question":"What are the differences between functional and class components?",
+        "answers":"Before the introduction of Hooks in React, functional components were called stateless components and were behind class components on a feature basis. After the introduction of Hooks, functional components are equivalent to class components."
+    },
+    {
+        "id":5,
+        "question":"What are props in React?",
+        "answers":"The props in React are the inputs to a component of React. They can be single-valued or objects having a set of values that will be passed to components of React during creation by using a naming convention that almost looks similar to HTML-tag attributes. We can say that props are the data passed from a parent component into a child component."
+    }
+]
