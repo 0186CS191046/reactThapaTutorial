449419e234d287e920b37a006130d390fc51ddfb
0186CS191046 <kajal.kumari@ens.enterprises>
2025-05-03 22:25:46 +0530

    useContext

--- src/CompB.jsx
+++ src/CompB.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React ,{useContext} from "react";
 import CompC from "./CompC"
+import {Fname,Lname} from "./App"
 
 const CompB = ()=> {
-    return(<>
-        <CompC/>
+    const fname = useContext(Fname)
+    const lname = useContext(Lname)
+    return(
+    <>
+        {/* <CompC/> */}
+        <h1>Hello {fname} {lname} </h1>
     </>)
 }
 
