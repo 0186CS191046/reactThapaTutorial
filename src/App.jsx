4cab453a7b67459fb81ac24e148e51120de80026
0186CS191046 <kajal.kumari@ens.enterprises>
2025-06-04 22:45:33 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.css"
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
+import {Routes , Route} from "react-router-dom"
 
 const App = () => {
-   const [fullName, setFullName] = useState({
-      fname: "",
-      lname: "",
-      email: "",
-      phone:""
-   })
-
-   const inputEvent = (e) => {
-      let { name, value } = e.target
-
-      setFullName((preValue) => {
-         console.log("logValue--",preValue);
-         return {
-            ...preValue,
-            [name] : value
-      }
-      })
-   }
-
-   const submitForm = (event) => {
-      event.preventDefault()
-   }
-
-   return (
-      <>
-         <form onSubmit={submitForm}>
-            <div className="block">
-               <h1>Hello {fullName.fname} {fullName.lname}</h1>
-               <p>{fullName.email}</p>
-               <p>{fullName.phone}</p>
-               <input type="text" placeholder="Enter your first_name" onChange={inputEvent} name="fname" value={fullName.fname} /><br />
-               <input type="text" placeholder="Enter your last_name" onChange={inputEvent} name="lname" value={fullName.lname} /><br />
-               <input type="email" placeholder="Enter your email" onChange={inputEvent} name="email" value={fullName.email} /><br />
-               <input type="number" placeholder="Enter your phone_number" onChange={inputEvent} name="phone" value={fullName.phone} />
-               <button type="submit">Click me ☺</button>
-            </div>
-         </form>
-      </>
-   )
+   
+return (
+  <>
+  <Routes>
+    <Route path="/" element= {<About/>}/>
+    <Route path="/contact" element= {<Contact/>}/>
+    <Route path="/contact/name" element={<Error/>}/>
+  </Routes>
+  {/* <About/> */}
+  {/* <Contact/> */}
+  </>
+)
 }
 
 export default App
